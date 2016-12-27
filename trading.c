c806bec875d1dbe32e5af9c551907583922e9139
Nick Pham <namp@kth.se>
2016-12-27 15:04:20 +0100

    trading

---
+++ trading.c
@@ -0,0 +1,9 @@
+#include <string.h>
+#include <thread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+  println("Hi Nick! This is producer-consumer program with multiple threads");
+  
+}
