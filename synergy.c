d7cf1e3894056e597fc391e7255bf449fb255d0a
Nick Pham <namp@kth.se>
2016-12-28 17:46:47 +0100

    Adding threads, modify data structures

--- synergy.c
+++ synergy.c
@@ -25,53 +25,58 @@ void* fighting(void *ptr){
 }
 
 int main(int argc, char *argv[]){
-  //if(argc != 3){
-    //perror("Incorrect number of arguments (3)");
-    //puts("Remember to include -pthread flag during COMPILING");
-    //puts("\n Usage: ./a.out <NUM_THREADS> <MESSAGE> \n");
-    //exit(1);
-  //}
-  printf("Hi there! This is a multi-threaded C program where users indicate their desired number of threads and a message. The message length is then divided equally among the threads to increase every character by 1\n\n");
-  puts("Remember to include -pthread flag during COMPILING");
-  size_t NUM_THREADS = 4;//atoi(argv[1]);
+  if(argc != 3){
+    perror("Incorrect number of arguments (3)");
+    puts("Remember to include -pthread flag during COMPILING");
+    puts("\n Usage: ./a.out <NUM_THREADS> <MESSAGE> \n");
+    exit(1);
+  }
+  printf("\nHi there! This is a multi-threaded C program where users indicate their desired number of threads and a message (with no space in between)\nThe message length is then divided equally among the threads to increase every character by 1\n");
+  puts("Remember to include -pthread flag when COMPILING\n");
+  size_t NUM_THREADS = atoi(argv[1]);
   pthread_t solders[NUM_THREADS];
-  char *mess = "Long message";
+  char mess[strlen(argv[2])];
+
+  
+  strcpy(mess,argv[2]);
   size_t weight = strlen(mess);
 
   size_t rem = weight % NUM_THREADS;
   
   //considering edge cases
   //diving the message accoring to num_threads and weight
-  Info *box[1000];
+  Info *box;
   if(weight >= NUM_THREADS){
-    //box = malloc(1000000);
-    size_t j=0;
-    for(;j< NUM_THREADS-1;j++){
-      puts("a messaage");
-      box[j]->data = mess+ j*(weight/NUM_THREADS);
-      box[j]->exec_times = weight/NUM_THREADS;
+    box = malloc(NUM_THREADS* sizeof(Info));
+    for(size_t j=0;j< NUM_THREADS;j++){
+      box[j].data = mess+ j*(weight/NUM_THREADS);
+      box[j].exec_times = weight/NUM_THREADS;
     }
-    puts("a messaage");
-    box[j]->data = mess+ j*(weight/NUM_THREADS);
-    if(weight%NUM_THREADS ==0) box[j]->exec_times = weight/NUM_THREADS;
-    else box[j]->exec_times = weight%NUM_THREADS;
+    if(weight%NUM_THREADS !=0) box[NUM_THREADS-1].exec_times += weight%NUM_THREADS;
   }else{
-    //box = malloc(weight* sizeof(Info*));
+    box = malloc(weight* sizeof(Info));
     for(size_t j=0;j< weight;j++){
-      box[j]->data = mess+ j;
-      box[j]->exec_times = 1;
+      box[j].data = mess+ j;
+      box[j].exec_times = 1;
     }
+    NUM_THREADS = weight;
   }
   //sending threads to encode the message
   for(size_t i = 0;i<NUM_THREADS;i++){
-    pthread_create(&solders[i],NULL,fighting, (void*)box[i]);
+    pthread_create(&solders[i],NULL,fighting, (void*)&box[i]);
   }
   for(size_t i =0; i<NUM_THREADS;i++){
     pthread_join(solders[i],NULL);
   }
 
   //free(box);
-  printf("Encoded message:  %s\n",mess);
-
+  printf("ENCODED MESSAGE IS: %s\n",mess);
+  
+  //DECODE
+  //for(size_t i =0;i<weight;i++){
+    //mess[i]=mess[i]-1;
+  //}
+  //printf("DECODED MESSAGE IS: %s\n",mess);
+  puts("");
   return 0;
 }
